724acccf2ebf3cfbe0b0d7311134e93284673509
wanjirukinyira-alt <wanjirukinyira@gmail.com>
2026-08-14 18:15:27 +0300

    made changes on layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "../components/Navbar";
-
 import "./globals.css";
 import ThemeProvider from "@/components/Themeprovider";
 
@@ -23,16 +22,14 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
       <body className="min-h-full flex flex-col">
         <ThemeProvider>
           <Navbar />
           {children}
         </ThemeProvider>
       </body>
-      <body className="min-h-full flex flex-col">{children}</body>
-      <Navbar />
-
     </html>
   );
 }
